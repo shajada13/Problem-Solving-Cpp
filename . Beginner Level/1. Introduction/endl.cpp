6c7b31753f6ff89897db89b65eced6fd61e6e71f
shajada13 <shajadamasum511@gmail.com>
2025-02-27 00:23:00 +0600

    update

---
+++ . Beginner Level/1. Introduction/endl.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+   // cout << "HELLO";NOT CORRECT TO NEW LINE CREATE
+    cout << "HELLO"<<endl;
+    cout << "WORLD";
+    return 0;
+}
