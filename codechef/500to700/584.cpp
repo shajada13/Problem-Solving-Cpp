cf3c05bf9bd1a7db660c30b5f9035e47ca41f5da
shajada13 <shajadamasum511@gmail.com>
2025-08-08 15:41:59 +0600

    adding 585

---
+++ codechef/500to700/584.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    for (int i = 0; i < T; i++) {
+        int N[3];
+        for (int j = 0; j < 3; j++) {
+            cin >> N[j];
+        }
+
+
+
+        int minTemp = max(N[0], N[2]);   // max function return max value , like a = 10 , b = 20 , then function return 20 ; 
+        int maxTemp = N[1];             
+
+        if (minTemp <= maxTemp) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
---
+++ codechef/500to700/585.cpp
@@ -0,0 +1,18 @@
+//584 code force 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+ 
+    int T , X ;
+    cin >> T ;
+    
+    for (int i = 0 ; i < T ; i ++){
+        cin >> X ;
+        if ( X > 50){
+            cout << "RIGHT" << endl;
+        }else {
+            cout << "LEFT" << endl;
+        }
+    }
+}
