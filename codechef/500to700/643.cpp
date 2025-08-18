28316df580e62410e551dc5565ee38d1c4dbd8bd
shajada13 <shajadamasum511@gmail.com>
2025-08-18 14:44:05 +0600

    added new 643

---
+++ codechef/500to700/643.cpp
@@ -0,0 +1,28 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int T;
+    cin >> T ;
+    
+    while (T--){
+        int X , Y ;
+        cin >> X >> Y ;
+        
+        int litres = Y * 2 ;
+        int  bath = X / litres ;
+        
+        // if (X < litres ){
+        //     cout << 0 << endl;
+        // }else{
+        //     cout << bath << endl;
+        // }
+        
+        
+        cout << ((X < litres) ? 0 : bath ) << endl; 
+        //simple proccess to trinary conditional operator ( if else short type )
+    }
+}
