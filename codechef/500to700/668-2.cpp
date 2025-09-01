94a4bac7464b50f5ef4cea1269f9960d7da9e05e
shajada13 <shajadamasum511@gmail.com>
2025-09-01 15:27:27 +0600

    added

---
+++ codechef/500to700/668-2.cpp
@@ -0,0 +1,28 @@
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N;
+        cin >> N;
+        
+        int squareRoot = sqrt(N); 
+        
+       cout << squareRoot << endl;
+
+        // int squareRoot = 0;
+
+        // for (int i = 1; i*i <= N; i++) {
+        //     squareRoot = i;
+        // }
+
+        // cout << squareRoot << endl;
+    }
+
+    return 0;
+}
